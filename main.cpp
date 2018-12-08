ec3a85f26486d73386bf858e3cbfa3551f6b3d66
AKHMAD SAIFUL ANAM <45529658+akhmadaan@users.noreply.github.com>
2018-12-09 00:40:06 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+    system("color 2");
+
+
+	int usia;
+	cout<<"berapa usia anda bro???:"<<endl;
+	cin>>usia;
+	if (usia<17)
+	cout<<"Anda belum dewasa"<<endl;
+	else
+        cout<<"Anda sudah dewasa"<<endl;
+
+
+}
